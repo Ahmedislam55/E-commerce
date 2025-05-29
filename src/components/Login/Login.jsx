9fe92a422d07f70e96764cfe4ac207ebd3f12060
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-05-29 16:44:15 +0300

    Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -156,7 +156,7 @@ function Login() {
               </Link>{" "}
             </span>
           </div>
-          <div className="flex justify-end">
+          <div className="md:flex justify-end">
             <button
               type="submit"
               className="bg-main border-0 px-4 py-2 rounded hover:bg-green-800 hover:border-main hover:border transation duration-700 mt-4 text-white flex items-center justify-center"
